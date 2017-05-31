1ae635c4cd4ef1fd5bcf5df76d0a79be99951655
user <user@YH-20170224NRDM>
2017-05-31 15:06:27 +0800

    添加项目文件。

---
+++ WindowsApplication4/Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Info
+        Me.TextBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.TextBox1.Location = New System.Drawing.Point(217, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(419, 309)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 128)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "启动"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(15, 170)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "终止"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(217, 327)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(419, 42)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(643, 327)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(137, 42)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "群体发送"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(13, 209)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox3.Size = New System.Drawing.Size(188, 152)
+        Me.TextBox3.TabIndex = 5
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(60, 12)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(112, 21)
+        Me.TextBox4.TabIndex = 6
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(60, 68)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(112, 21)
+        Me.TextBox5.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(17, 12)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Ip"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 12)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Port"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(642, 12)
+        Me.TextBox6.Multiline = True
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox6.Size = New System.Drawing.Size(138, 309)
+        Me.TextBox6.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(777, 383)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox6 As TextBox
+End Class
---
+++ WindowsApplication4/Form1.vb
@@ -0,0 +1,199 @@
+﻿Imports System
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.Threading
+Imports Microsoft.VisualBasic
+
+Public Class Form1
+
+    Dim listen_thread As Thread
+    Dim listener As Socket
+    Public allDone As New ManualResetEvent(False)
+    Dim Thread_listen As Thread
+    ' State object for reading client data asynchronously
+    Public Class StateObject
+        ' Client  socket.
+        Public workSocket As Socket = Nothing
+        ' Size of receive buffer.
+        Public Const BufferSize As Integer = 1024
+        ' Receive buffer.
+        Public buffer(BufferSize) As Byte
+        ' Received data string.
+        Public sb As New StringBuilder
+        Public size As Int32
+    End Class 'StateObject
+
+    Dim So As New ArrayList
+    Dim thread_lock As Object = New Object()
+    Dim Time As Timers.Timer
+
+    Public Sub Time_hande()
+        Dim str As String = ""
+        SyncLock thread_lock '////////////
+            For Each member As Socket In So
+                str = str & member.RemoteEndPoint.ToString() & vbNewLine
+            Next
+        End SyncLock
+        TextBox6.Text = str
+    End Sub
+
+    Public Sub Accept_callback(ar As IAsyncResult)
+        allDone.Set()
+
+        Dim waitDone As New ManualResetEvent(False)
+        Dim socketi As Socket
+
+        Try
+            socketi = listener.EndAccept(ar)
+        Catch ie As Exception
+            Exit Sub
+        End Try
+
+        TextBox3.AppendText(vbNewLine & "加入一个" & socketi.RemoteEndPoint.ToString)
+        TextBox3.ScrollToCaret()
+        Dim State As New StateObject()
+        State.workSocket = socketi
+        SyncLock thread_lock '/////////////////////
+            So.Add(socketi)
+        End SyncLock
+        State.size = 0
+        Dim err As Int32 = 0
+        While 1
+            Try
+                err = socketi.Receive(State.buffer)
+            Catch e As Exception
+                TextBox3.AppendText(vbNewLine & "客户端终止")
+                TextBox3.ScrollToCaret()
+                socketi.Close()
+                SyncLock thread_lock
+                    So.Remove(socketi)
+                End SyncLock
+                Exit Sub
+            End Try
+            If err > 0 Then
+                State.sb.Clear()
+                State.sb.Append(Encoding.ASCII.GetString(State.buffer, 0, err))
+                Dim str As String
+                str = State.sb.ToString()
+                TextBox1.AppendText(vbNewLine & str)
+                TextBox1.ScrollToCaret()
+            End If
+        End While
+    End Sub
+
+    Public Sub listen_handle()
+        TextBox3.AppendText(vbNewLine & "服务器启动")
+        TextBox3.ScrollToCaret()
+        While 1
+
+            allDone.Reset()
+            Try
+                listener.BeginAccept(New AsyncCallback(AddressOf Accept_callback), listener)
+            Catch e As Exception
+                Exit Sub
+            End Try
+            allDone.WaitOne()
+        End While
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim listenr As New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+        Dim localEndpoint As IPEndPoint
+        Dim Ip As String
+        Dim Port As String
+
+        Ip = TextBox4.Text()
+        If Ip = "" Then
+            MsgBox("please ensure your ip")
+            Return
+        End If
+        Port = TextBox5.Text()
+        If Port = "" Then
+            MsgBox("please ensure your port")
+            Return
+        End If
+
+        localEndpoint = New IPEndPoint(IPAddress.Parse(Ip), Port)
+        listener = listenr
+        listenr.Bind(localEndpoint)
+        listenr.Listen(1024)
+        listener = listenr
+        Dim Th As Thread
+        Th = New Thread(AddressOf listen_handle)
+        Thread_listen = Th
+        Th.Start()
+        Time = New Timers.Timer(500)
+        AddHandler Time.Elapsed, AddressOf Time_hande
+        Time.Start()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim str As String = TextBox2.Text
+        If str = "" Then
+            MsgBox("please input data")
+            Return
+        End If
+        Dim encText As New System.Text.UTF8Encoding()
+        Dim btText() As Byte
+        btText = encText.GetBytes(str)
+        SyncLock thread_lock
+            For Each member As Socket In So
+                Try
+                    member.Send(btText)
+                Catch pe As Exception
+                    TextBox1.AppendText("send failed" & member.RemoteEndPoint.ToString())
+                End Try
+            Next
+        End SyncLock
+        TextBox1.AppendText(vbNewLine & str)
+        TextBox1.ScrollToCaret()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Thread_listen.Abort()
+        Catch ex As Exception
+
+        End Try
+        listener.Close()
+        SyncLock thread_lock
+            For Each member As Socket In So
+                Try
+                    member.Close()
+                Catch ie As Exception
+                End Try
+
+            Next
+        End SyncLock
+        Time.Stop()
+        TextBox3.AppendText(vbNewLine & "服务器关闭")
+        TextBox3.ScrollToCaret()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox4.Text = "127.0.0.1"
+        TextBox5.Text = "8888"
+    End Sub
+
+    Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+        Try
+            Thread_listen.Abort()
+        Catch ex As Exception
+
+        End Try
+        listener.Close()
+        For Each member As Socket In So
+            Try
+                member.Close()
+            Catch ie As Exception
+            End Try
+
+        Next
+        Try
+            Time.Stop()
+        Catch fe As Exception
+        End Try
+    End Sub
+End Class
